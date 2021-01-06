efbf698802bc7042c451c5b2e99934774a46c3d5
julian <julian@onrewind.com>
2021-01-06 17:18:51 +0100

    sic commit

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -1,4 +1,13 @@
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:27105");
+
 function TEST() {
+  console.log(socket, "--> socket");
+
+  socket.on("connect", () => {
+    console.log("connect");
+  });
+
   return (
     <>
       <h1 style={{ color: "white", textAlign: "centers" }}>Socket with Zeng</h1>
